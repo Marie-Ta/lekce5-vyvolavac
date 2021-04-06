da3a8a37f06422020140173f8a37e222102ef849
Marie Tarantova <marie.tarantova@gmail.com>
2021-04-06 19:12:12 +0200

    Odebírání jmen z pole, zobrazení jmen ve vylosovaných

--- main.js
+++ main.js
@@ -32,17 +32,17 @@ let tazenaJmena = [];
 function tahnoutJmeno() {
     if (jmena.length === 0) {
         return;
-    }
+    } //pokud je délka pole 0, tak se vrať; return znamená, že funkce končí a něco vrací, v tomto případě nic nevrací, jen funkce končí
 
     // Generujeme náhodný index
     let vyherniIndex = Math.floor(Math.random() * jmena.length); 
-    console.log(vyherniIndex);
-
+    
     // Získáme výherní jméno na patřičném indexu
     let vyherniJmeno = jmena[vyherniIndex];
-    console.log(vyherniJmeno);
-
+    
     // Vyřadíme vylosované jméno z osudí
+    jmena.splice(vyherniIndex, 1);
+    console.log(jmena.length);
 
     // Výherní jméno si uložíme do pole k ostatním výherním
     
